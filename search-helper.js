34424002aa1699bda4ee236fe9af7a80356b7001
xhable <evgeny@zgursky.tk>
2022-11-17 00:58:49 +0300

    .gitignore added

---
+++ search-helper.js
@@ -0,0 +1,11 @@
+/********************************
+ *                              *
+ *    Smart Filtered Search     *
+ *            BGTU              *
+ *            2022              *
+ *                              *
+ ********************************/
+
+class SearchHelper {
+  constructor() {}
+}
